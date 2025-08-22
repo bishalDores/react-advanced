550d1b90d03544f9e99245507067318e9ea40b7c
bishal <bishalpd@gmail.com>
2025-08-22 15:31:44 +0600

    eslint fix2

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
-import featureSliced from "@conarti/eslint-plugin-feature-sliced";
-import importPlugin from "eslint-plugin-import";
+
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -13,10 +12,6 @@ export default tseslint.config(
   [
     globalIgnores(["dist"]),
     {
-      plugins: {
-        "@conarti/feature-sliced": featureSliced,
-        import: importPlugin,
-      },
       files: ["**/*.{ts,tsx}"],
       extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs["recommended-latest"], reactRefresh.configs.vite],
       languageOptions: {
@@ -24,59 +19,12 @@ export default tseslint.config(
         globals: globals.browser,
       },
       rules: {
-        ...featureSliced.configs.recommended.rules,
-        "import/order": [
-          "error",
-          {
-            groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-            pathGroups: [
-              {
-                pattern: "@/app/**",
-                group: "internal",
-                position: "before",
-              },
-              {
-                pattern: "@/pages/**",
-                group: "internal",
-                position: "before",
-              },
-              {
-                pattern: "@/widgets/**",
-                group: "internal",
-                position: "before",
-              },
-              {
-                pattern: "@/features/**",
-                group: "internal",
-                position: "before",
-              },
-              {
-                pattern: "@/entities/**",
-                group: "internal",
-                position: "before",
-              },
-              {
-                pattern: "@/shared/**",
-                group: "internal",
-                position: "before",
-              },
-            ],
-            pathGroupsExcludedImportTypes: ["builtin"],
-            "newlines-between": "always",
-            alphabetize: {
-              order: "asc",
-              caseInsensitive: true,
-            },
-          },
-        ],
-
-        "import/first": "error",
-        "import/newline-after-import": "error",
-        "import/no-duplicates": "error",
         "@typescript-eslint/no-unused-vars": [
           "error",
           {
             argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            ignoreRestSiblings: true,
           },
         ],
       },
